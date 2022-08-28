9b9dd86631b58fe8a33d04097bb7d099acf7dda2
Anita Pajic <anitaapajic@gmail.com>
2022-08-28 22:14:04 +0200

    Added SSTable and BloomFilter

---
+++ SSTable/BloomFilter.go
@@ -0,0 +1,124 @@
+package SSTable
+
+import (
+	"encoding/gob"
+	"github.com/TamaraDzambic/NASP-projekat/WriteAheadLog"
+	"hash"
+	"math"
+	"os"
+	"time"
+
+	"github.com/spaolacci/murmur3"
+)
+
+
+type BloomFilter struct {
+	n     uint          // broj elemenata
+	p 	  float64       // verovatnoća greške
+	m     uint          // veličina bitseta
+	k     uint          // broj hash funkcija
+	set   []byte        // set sa bitovima
+	hashs []hash.Hash32 // hash funkcije
+	TimeConst uint
+}
+
+func NewBF(n uint, p float64) *BloomFilter {
+	m := M(int(n), p)
+	k := K(int(n), m)
+	hashs, tc := Hash(k)
+	return &BloomFilter{n, p, m, k, make([]byte, m), hashs, tc}
+}
+
+func (bf *BloomFilter) Add(elem WriteAheadLog.Entry) {
+	for _, hashF := range bf.hashs {
+		i := HashElement(hashF, elem.Key, bf.m)
+		bf.set[i] = 1
+	}
+}
+
+func (bf *BloomFilter) Find(elem string) bool {
+	for _, hashF := range bf.hashs {
+		i := HashElement(hashF, elem, bf.m)
+		if bf.set[i] != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func HashElement(hashF hash.Hash32, elem string, m uint) uint32 {
+	_, err := hashF.Write([]byte(elem))
+	if err != nil {
+		panic(err)
+	}
+	i := hashF.Sum32() % uint32(m)
+	hashF.Reset()
+	return i
+}
+
+func M(n int, p float64) uint {
+	return uint(math.Ceil(float64(n) * math.Abs(math.Log(p)) / math.Pow(math.Log(2), float64(2))))
+}
+
+func K(n int, m uint) uint {
+	return uint(math.Ceil((float64(m) / float64(n)) * math.Log(2)))
+}
+
+func Hash(k uint) ([]hash.Hash32, uint) {
+	var h []hash.Hash32
+	ts := uint(time.Now().Unix())
+	for i := uint(0); i < k; i++ {
+		h = append(h, murmur3.New32WithSeed(uint32(ts+1)))
+	}
+	return h, ts
+}
+
+func CopyHashFunctions(k uint, tc uint) []hash.Hash32 {
+	var h []hash.Hash32
+	for i := uint(0); i < k; i++ {
+		h = append(h, murmur3.New32WithSeed(uint32(tc+1)))
+	}
+	return h
+}
+
+func writeBloomFilter(filename string, bf *BloomFilter) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(bf)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readBloomFilter(filename string) (bf *BloomFilter) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	decoder := gob.NewDecoder(file)
+	bf = new(BloomFilter)
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		return nil
+	}
+
+	for {
+		err = decoder.Decode(bf)
+		if err != nil {
+			//fmt.Println(err)
+			break
+		}
+		//fmt.Println(*bf)
+	}
+	bf.hashs = CopyHashFunctions(bf.k, bf.TimeConst)
+	return
+}
+
---
+++ SSTable/SStable.go
@@ -0,0 +1,198 @@
+package SSTable
+
+import (
+	"bufio"
+	"encoding/binary"
+	"github.com/TamaraDzambic/NASP-projekat/WriteAheadLog"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+type SSTable struct {
+	indexMap 	map[string]uint64
+	data         *os.File
+	index        *os.File
+	summary 	 *os.File
+	bf  BloomFilter
+	//MerkleTree   MerkleTree.MerkleRoot
+}
+
+func  NewSST(datasize uint, dataFN string, indexFN string, summaryFN string) *SSTable{
+	sstable :=	SSTable{indexMap: make(map[string]uint64), summary: CreateFile(summaryFN), data: CreateFile(dataFN), index: CreateFile(indexFN), bf: *NewBF(datasize, 0.01)}
+	defer CloseFiles(sstable)
+	return &sstable
+}
+
+func CreateFile(fn string) *os.File {
+	err := os.MkdirAll(filepath.Dir(fn), 0777)
+	fp, err := os.Create(fn)
+	if err != nil {
+		panic(err)
+	}
+	return fp
+}
+
+func (table *SSTable) createIndex(){
+	//sortiraj index mapu i upisi u index i summary
+	keys := make([]string, 0, len(table.indexMap))
+	for k := range table.indexMap{
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	table.index = CreateFile(table.index.Name())
+	table.summary = CreateFile(table.summary.Name())
+
+	WriteKey(table.summary, keys[0])
+	WriteKey(table.summary, keys[len(keys)-1])
+
+	for _, k := range keys {
+		indexOffset := FileSize(table.index)
+		WriteKey(table.index, k)
+		WriteOffset(table.index, table.indexMap[k])
+		WriteKey(table.summary, k)
+		WriteOffset(table.summary, indexOffset)
+	}
+
+	CloseFiles(*table)
+}
+
+func (table *SSTable) WriteData(data []WriteAheadLog.Entry) {
+	table.data, _ = os.OpenFile(table.data.Name(), os.O_APPEND, 0777)
+
+	for _, entry := range data {
+		offset := FileSize(table.data)
+		table.data.Write(entry.Encode())
+        table.indexMap[entry.Key] = offset
+
+		table.bf.Add(entry)
+	}
+	table.createIndex()
+
+	writeBloomFilter("bloomFile", &table.bf)
+}
+
+
+func (table *SSTable) Find( key string) (entry WriteAheadLog.Entry, found bool) {
+	table.data, _ = os.OpenFile(table.data.Name(), os.O_RDONLY, 0777)
+	table.index, _ =os.OpenFile(table.index.Name(), os.O_RDONLY, 0777)
+	table.summary, _ =os.OpenFile(table.summary.Name(), os.O_RDONLY, 0777)
+	defer CloseFiles(*table)
+
+	if !table.bf.Find(key) {
+		found = false
+		return
+	}
+
+	readerS := bufio.NewReader(table.summary)
+	readerI := bufio.NewReader(table.index)
+
+	minKey := ReadKey(readerS)
+	maxKey := ReadKey(readerS)
+	
+	if minKey > key || maxKey < key {
+		found = false
+		return
+	}
+	for {
+		keyS := ReadKey(readerS)
+		if keyS == "" {
+			found = false
+			break
+		}
+		offset, _ := ReadOffset(readerS)
+		if key < keyS {
+			found = false
+			return
+		}
+		if key == keyS {
+			table.index.Seek(int64(offset), 0)
+			ReadKey(readerI)
+			dataOffset, _ := ReadOffset(readerI)
+
+			table.data.Seek(int64(dataOffset), 0)
+
+			readerD := bufio.NewReader(table.data)
+			err := os.ErrExist
+			err, entry = WriteAheadLog.Decode(readerD)
+			if err == nil {
+				found = true
+				return 
+			}
+
+		}
+	}
+	found = false
+	return
+}
+
+
+func WriteKey(fp *os.File, key string) {
+	keyBytes := []byte(key)
+	keyLen := uint64((len(keyBytes)))
+	lenBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(lenBytes, keyLen)
+
+	bytes := make([]byte, 0, 8+keyLen)
+	bytes = append(bytes, lenBytes...)
+	bytes = append(bytes, keyBytes...)
+	fp.Write(bytes)
+}
+
+
+func ReadKey(reader *bufio.Reader) string {
+	keyLenBytes := make([]byte, 8)
+	_, err := reader.Read(keyLenBytes)
+	if err != nil {
+		panic(err)
+	}
+	keyLen := binary.LittleEndian.Uint64(keyLenBytes)
+
+	keyBytes := make([]byte, keyLen)
+	_, err = reader.Read(keyBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(keyBytes)
+}
+
+func WriteOffset(fp *os.File, value uint64) {
+	bytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bytes, value)
+	fp.Write(bytes)
+}
+
+func ReadOffset(reader *bufio.Reader) (uint64, error) {
+	bytes := make([]byte, 8)
+	_, err := reader.Read(bytes)
+	if err == io.EOF {
+		return 0, err
+	} else {
+		if err != nil {
+			panic(err)
+		}
+	}
+	return binary.LittleEndian.Uint64(bytes), nil
+}
+
+
+func CloseFiles(sst SSTable) {
+	sst.data.Close()
+	sst.index.Close()
+	sst.summary.Close()
+}
+
+func FileSize(fp *os.File) uint64 {
+	fi, err := fp.Stat()
+	if err != nil {
+		panic(err)
+	}
+	return uint64(fi.Size())
+}
+
+
+
+
