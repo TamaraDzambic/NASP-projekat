6b79d4c5add75cabc772ca5cfec9f1e51c6b205d
Anja Petković <anjapetkovic92@gmail.com>
2022-08-29 02:56:59 +0200

    Memtable extended with flush function

--- memtable/Memtable.go
+++ memtable/Memtable.go
@@ -1,50 +1,50 @@
 package memtable
+
+import (
+	"github.com/TamaraDzambic/NASP-projekat/SSTable"
+)
+
 type Memtable struct {
-	SkipList SkipList
-	capacity int64
-	currentCapacity int64
+	SkipList *SkipList
+	capacity int
+	SSTable SSTable.SSTable
 }
 
 
-func (memtable *Memtable) Set(key string, value []byte, tombstone bool) bool {
-	if memtable.currentCapacity < memtable.capacity {
-		return memtable.SkipList.Set(key, value, tombstone)
+func (memtable *Memtable) Set(key string, value []byte, tombstone bool) {
+	if memtable.SkipList.size <= memtable.capacity {
+		memtable.SkipList.Set(key, value, tombstone)
 	} else {
-		//flush
-		return false
+		memtable.Flush()
+		memtable.SkipList.Set(key, value, tombstone)
 	}
 }
 
 func (memtable *Memtable) Get(key string) *Element {
-	var element *Element
-	element = memtable.SkipList.Get(key)
+	element := memtable.SkipList.Get(key)
 	if element != nil{
 		return element
 	}
 	return nil
 }
 
-func (memtable *Memtable) Remove(key string, value []byte) bool {
-	var element *Element
-	element = memtable.SkipList.Get(key)
-	if element != nil{
-		element.tombstone = true
-		// videti da li izbrisati skroz ili upisati sa novim flegom
-		memtable.SkipList.Remove(key)
-		return true
-	}
-	return false
-
+func (memtable *Memtable) Remove(key string){
+	memtable.SkipList.Remove(key)
 }
 
-func NewMemtable(maxCapacity int64) *Memtable {
+func NewMemtable(maxCapacity int) *Memtable {
 	return &Memtable{
-		SkipList: *createSkipList(int(maxCapacity)),
+		SkipList: CreateSkipList(maxCapacity),
 		capacity: maxCapacity,
-		currentCapacity: 0,
+		SSTable: *SSTable.NewSST(uint(maxCapacity), "SSTable\\files\\data", "SSTable\\files\\index", "SSTable\\files\\summary"),
 	}
 }
+
 
 func (memtable *Memtable) Flush(){
-	// funkcija flush
+	elements := memtable.SkipList.GetElements()
+	memtable.SSTable.WriteData(elements)
+	memtable.SkipList.PrintList()
+	memCap := memtable.capacity
+	*memtable = *NewMemtable(memCap)
 }
