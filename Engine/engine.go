6659e0d45abe273f012e4e17087919b84606ef4f
Tamara Dzambic <dzambic.sv43.2020@uns.ac.rs>
2022-09-01 20:26:58 +0200

    Improved display

--- Engine/engine.go
+++ Engine/engine.go
@@ -150,6 +150,9 @@ func (engine *Engine) delete(key string, tombstone byte) bool{
 	entry, f := engine.get(key)
 	if f == true {
 		if engine.wal.AddEntry(entry.Key, entry.Value, 1){
+			entry.Tombstone = 1
+			engine.memtable.Remove(entry.Key)
+			engine.cache.Put(*entry)
 			return engine.put(key, entry.Value, 1)
 		}
 	}
@@ -162,20 +165,30 @@ func (engine *Engine) get(key string) (*WriteAheadLog.Entry, bool){
 	if element!=nil{
 		x := WriteAheadLog.CreateEntry(element.Key, element.Value, Memtable.BoolToByte(element.Tombstone))
 		engine.cache.Put(x)
-		fmt.Println("u memtable/skiplist")
+		if element.Tombstone!=true{
+			fmt.Println(element.Key, string(element.Value), element.Tombstone)
+		}else{
+			fmt.Println(element.Key, string(element.Value), element.Tombstone, "deleted!")
+		}
 		return &x, true
 	} else {
 		var value = engine.cache.Get(key)
 		if value!=nil{
-			fmt.Println("u cache-u")
+			//fmt.Println("u cache-u")
 			x := WriteAheadLog.CreateEntry(key, value, 0)
+			fmt.Println(x.Key, string(x.Value), x.Tombstone)
 			engine.cache.Put(x)
 			return &x, true
 		} else{
 			var entry, f = engine.sstable.Find(key)
 			if f!=false{
-				fmt.Println("u sstable")
+				//fmt.Println("u sstable")
 				engine.cache.Put(entry)
+				if entry.Tombstone!=1{
+					fmt.Println(entry.Key, string(entry.Value), entry.Tombstone)
+				}else{
+					fmt.Println(entry.Key, string(entry.Value), entry.Tombstone, "deleted!")
+				}
 				return &entry, f
 			}
 		}
